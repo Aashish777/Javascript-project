406802871003b837dd31548d63ee6592e4079654
Aashish777 <aashishb879@gmail.com>
2023-10-09 12:16:32 +0545

    Admin Panel

---
+++ Admin_Panel/script.js
@@ -0,0 +1,12 @@
+ const body = document.querySelector("body")
+       modeToggle = body.querySelector(".mode-toggle")
+       sideBar = body.querySelector("nav")
+       sidebarToggle = body.querySelector(".sidebar-toggle")
+
+modeToggle.addEventListener("click", ()=>{
+    body.classList.toggle("dark");
+
+})
+sidebarToggle.addEventListener("click", ()=>{
+    sideBar.classList.toggle("close")
+})
